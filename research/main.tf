a0d0a899492c0b1a7dd2cdf5e5a00efcef03b2b6
AdamCavaliere <adam.cavaliere@gmail.com>
2018-11-09 16:17:01 -0600

    Update main.tf

--- research/main.tf
+++ research/main.tf
@@ -1,8 +1,33 @@
-data "terraform_remote_state" "research_subnet" {
+data "terraform_remote_state" "network" {
   backend = "atlas"
 
   config {
-    name = "${var.org}/${var.research_workspace}"
+    name = "${var.org}/${var.workspace_name}"
   }
 }
 
+data "aws_ami" "ubuntu" {
+  most_recent = true
+
+  filter {
+    name   = "name"
+    values = ["ubuntu/images/hvm-ssd/ubuntu-trusty-14.04-amd64-server-*"]
+  }
+
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+
+  owners = ["099720109477"] # Canonical
+}
+
+resource "aws_instance" "web" {
+  ami           = "${data.aws_ami.ubuntu.id}"
+  instance_type = "t2.micro"
+  subnet_id = "${data.terraform_remote_state.network.research_subnet_id}"
+
+  tags {
+    Name = "Research Instance"
+  }
+}
