7417394fd964a71eaeaa566d0deea37bd800f367
jelinn <jelinn@gmail.com>
2019-02-11 12:49:27 -0600

    Update dynamo.tf

--- dynamo.tf
+++ dynamo.tf
@@ -1,5 +1,9 @@
+variable "environment"{
+   type = "string"
+  }
+
 resource "aws_dynamodb_table" "basic-dynamodb-table" {
-  name           = ${var.environment}"-GameScores"
+  name           = "${var.environment}-GameScores"
   billing_mode   = "PROVISIONED"
   read_capacity  = 20
   write_capacity = 20
