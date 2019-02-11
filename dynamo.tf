57ff8fd6a3eaf8f650a8ffbbe3e532b7eeef23ce
jelinn <jelinn@gmail.com>
2019-02-11 11:57:18 -0600

    Update dynamo.tf

--- dynamo.tf
+++ dynamo.tf
@@ -24,7 +24,6 @@ resource "aws_dynamodb_table" "basic-dynamodb-table" {
   global_secondary_index {
     name               = "GameTitleIndex"
     hash_key           = "GameTitle"
-    range_key          = "TopScore"
     write_capacity     = 10
     read_capacity      = 10
     projection_type    = "INCLUDE"
