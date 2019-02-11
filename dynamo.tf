ee6d0727bfa16954bf2da7f72485a781ba406a69
AdamCavaliere <adam.cavaliere@gmail.com>
2019-02-11 11:45:23 -0600

    Update dynamo.tf

--- dynamo.tf
+++ dynamo.tf
@@ -16,11 +16,6 @@ resource "aws_dynamodb_table" "basic-dynamodb-table" {
     type = "S"
   }
 
-  attribute {
-    name = "TopScore"
-    type = "N"
-  }
-
   ttl {
     attribute_name = "TimeToExist"
     enabled        = false
