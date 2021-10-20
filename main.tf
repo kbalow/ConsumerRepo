0d04a6f1e6fec469fe73017fb8c86437b0e27066
kbalow <kbalow@gmail.com>
2021-10-20 09:35:53 -0500

    Update main.tf

--- main.tf
+++ main.tf
@@ -28,7 +28,7 @@ data "aws_ami" "ubuntu" {
 
 resource "aws_instance" "web" {
   ami           = data.aws_ami.ubuntu.id
-  instance_type = "t2.small"
+  instance_type = "t2.medium"
   subnet_id     = lookup(local.subnets, var.environment, "fail")
 
   tags = {
